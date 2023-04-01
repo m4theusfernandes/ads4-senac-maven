0c81cd6dd69b2e8fef27eb1042e729b973d0386b
m4theusfernandes <matheus_mfg@hotmail.com>
2023-03-31 22:58:58 -0300

    Atividades Do Dia 31/03/2023

--- src/main/java/br/senac/go/resources/PessoaResource.java
+++ src/main/java/br/senac/go/resources/PessoaResource.java
@@ -2,15 +2,33 @@ package br.senac.go.resources;
 
 import br.senac.go.domain.Pessoa;
 import br.senac.go.generics.GenericOperationsResource;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+@RestController //Fala que o controlador vai trabalhar com REST
+@RequestMapping(path = "/pessoa")
 public class PessoaResource implements GenericOperationsResource<Pessoa,Integer> {
+    private static final Logger LOGGER =
+            Logger.getLogger(PessoaResource.class.getName());
     @Override
-    public Pessoa post(Pessoa entity) {
-        return null;
-    }
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
+    public Pessoa post(@RequestBody @Validated Pessoa entity) {
 
+        LOGGER.log(Level.INFO,"Exemplo de imprimir", entity);
+
+        return entity;
+    }
     @Override
     public List<Pessoa> get() {
         return null;
